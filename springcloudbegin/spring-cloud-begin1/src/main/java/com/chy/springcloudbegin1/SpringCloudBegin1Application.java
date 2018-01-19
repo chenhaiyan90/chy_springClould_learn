8afeaf1611de5127e0924ae59a67e72983e0c979
chenhaiyan <595316059@qq.com>
2018-01-19 16:17:09 +0800

    节点监控检测等

--- springcloudbegin/spring-cloud-begin1/src/main/java/com/chy/springcloudbegin1/SpringCloudBegin1Application.java
+++ springcloudbegin/spring-cloud-begin1/src/main/java/com/chy/springcloudbegin1/SpringCloudBegin1Application.java
@@ -1,12 +1,35 @@
 package com.chy.springcloudbegin1;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
+
+@Slf4j
 @SpringBootApplication
 public class SpringCloudBegin1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudBegin1Application.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			log.info("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				log.info(beanName);
+			}
+
+		};
+	}
+
 }
